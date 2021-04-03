0cf547e085ffc697ad84043ed3d1cfed547d234e
wondrousWebWorks <alberts.johann@gmail.com>
2021-04-03 22:36:22 +0000

    Tests and function to test evens function before refactoring

--- evens.py
+++ evens.py
@@ -8,7 +8,22 @@ def even_number_of_evens(numbers):
     if the number of even numbers is even, return True
     """
 
-    return None
+    if isinstance(numbers, list):
+        if numbers == []:
+            return False
+        else:
+            evens = 0
+
+        for n in numbers:
+            if n % 2 == 0:
+                evens += 1
+
+        if evens:
+            return evens % 2 == 0
+        else:
+            return False
+    else:
+        raise TypeError("A list was not passed into the function")
 
 
 if __name__ == '__main__':
